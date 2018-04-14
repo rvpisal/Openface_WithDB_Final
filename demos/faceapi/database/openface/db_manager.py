02f8d012b1f1c6b9d0dbdebd7e3dfed9f172cf27
rohan <rvpisal@ilstu.edu>
2018-04-14 11:12:25 -0500

    Attendance code added

--- demos/faceapi/database/openface/db_manager.py
+++ demos/faceapi/database/openface/db_manager.py
@@ -11,6 +11,10 @@ Created on: 2016/1/14
 import os
 import sqlite3
 import logging
+import time
+import datetime
+from datetime import datetime,timedelta
+import calendar
 
 import faceapi
 from faceapi import exceptions
@@ -176,11 +180,49 @@ class DbManagerOpenface(DbManager):
 
         return rows
 
+    # def create_unique_db(self, name):
+    #     # conn = _sqlite3.connect(".db3")
+    #     time_stamp = sqlite3.datetime.datetime.now()
+    #
+    #     db_file = os.path.join(faceapi.BASE_DIR, "../web", "detected_faces.db3")
+    #
+    #     dir = os.path.dirname(db_file)
+    #     if not os.path.exists(dir):
+    #         os.makedirs(dir)
+    #
+    #
+    #     try:
+    #         with sqlite3.connect(db_file) as db:
+    #             cur = db.cursor()
+    #             cur.execute("Create table if not exists Names (name TEXT, Timestamp DATETIME)")
+    #             db.commit()
+    #             # cur.execute("select count(*) from (select name from Names where name =?)", ((hit['name']),))
+    #             cur2 = db.cursor()
+    #             # self._log.info(name)
+    #             cur2.execute("select count(*) from (select name from Names where name =?)", (name,))
+    #             name_counter = cur2.fetchone()
+    #
+    #             # self._log.info(name_counter)
+    #             if name_counter == (0,):
+    #                 # cur.execute("Insert into Names values(?,?)", (hit['name'], time_stamp))
+    #                 cur2.execute("Insert into Names values(?,?)", (name, time_stamp))
+    #                 db.commit()
+    #             else:
+    #                 # cur.execute("update Names set Timestamp=? where name=?", (time_stamp, (hit['name']),))
+    #                 cur2.execute("update Names set Timestamp=? where name=?", (time_stamp, name))
+    #                 db.commit()
+    #             cur.close()
+    #             cur2.close()
+    #     except sqlite3.Error as e:
+    #         self._log.error(str(e))
+    #         raise e
+
     def create_unique_db(self, name):
         # conn = _sqlite3.connect(".db3")
-        time_stamp = sqlite3.datetime.datetime.now()
+        time_stamp = datetime.now()
+        curr_date = datetime.date(datetime.now())
 
-        db_file = os.path.join(faceapi.BASE_DIR, "../web", "detected_faces.db3")
+        db_file = os.path.join(faceapi.BASE_DIR, "../web", "Attendance.db3")
 
         dir = os.path.dirname(db_file)
         if not os.path.exists(dir):
@@ -189,26 +231,103 @@ class DbManagerOpenface(DbManager):
 
         try:
             with sqlite3.connect(db_file) as db:
-                cur = db.cursor()
-                cur.execute("Create table if not exists Names (name TEXT, Timestamp DATETIME)")
-                db.commit()
-                # cur.execute("select count(*) from (select name from Names where name =?)", ((hit['name']),))
-                cur2 = db.cursor()
-                # self._log.info(name)
-                cur2.execute("select count(*) from (select name from Names where name =?)", (name,))
-                name_counter = cur2.fetchone()
 
-                # self._log.info(name_counter)
-                if name_counter == (0,):
-                    # cur.execute("Insert into Names values(?,?)", (hit['name'], time_stamp))
-                    cur2.execute("Insert into Names values(?,?)", (name, time_stamp))
-                    db.commit()
-                else:
-                    # cur.execute("update Names set Timestamp=? where name=?", (time_stamp, (hit['name']),))
-                    cur2.execute("update Names set Timestamp=? where name=?", (time_stamp, name))
-                    db.commit()
+                db.execute("CREATE TABLE if not exists Attendance(Course_id INTEGER,ULID TEXT, TimeStamp TEXT, Day TEXT,"
+                           " PRIMARY KEY(Course_id,Day))")
+                db.execute("CREATE TABLE if not exists Class_Schedule (Course_id INTEGER, Subject_name text, Day Text, "
+                            "Start_Time date, End_Time date, PRIMARY KEY(Course_id,Day))")
+                db.execute("CREATE TABLE if not exists Registration_Details (ULID TEXT, Course_id INTEGER, "
+                           "PRIMARY KEY(ULID,Course_id))")
+
+                cur = db.cursor()
+
+                cur.execute("Select Course_id from Registration_Details where ULID = ?", (name,))
+
+                reg_classes = list([x[0] for x in cur.fetchall()])
+
+                self._log.info(name)
+
+                self._log.info(reg_classes)
+                self._log.info(time_stamp)
+
+                params = '?'*len(reg_classes)
+
+                # print("Select Course_id from Class_Schedule where ? between Start_Time and End_Time and Day = ? and Course_id in ({})".format(','.join(params)))
+
+                courses_taken =  "Select Course_id from Class_Schedule where time(?) between time(Start_Time) and time(End_Time) and Day = ? and " \
+                                 "Course_id in ({})".format(','.join(params))
+
+
+                query_args = [time_stamp, calendar.day_name[(time_stamp).weekday()]]
+                query_args.extend(reg_classes)
+
+                cur.execute(courses_taken,query_args)
+                course_id = list ([x[0] for x in cur.fetchall()])
+
+
+                # classes = list([x[0] for x in cur.fetchall()])
+                # print ("Ongoing Classes: ", classes)
+                # self._log.info(classes)
+
+                # cur.execute("Select Course__id from Registration_Details where ULID = ?", (name,))
+                #
+                # reg_classes = list([x[0] for x in cur.fetchall()])
+                # print ("Registered Classes: ",reg_classes)
+                # self._log.info(reg_classes)
+
+                # sub_id = list(set(classes).intersection(reg_classes))
+                self._log.info(course_id)
+
+                if course_id:
+                    cur.execute("Select count(*) from (Select * from Attendance where course_id = ? and ULID = ? and "
+                                "date(TimeStamp) = ?)", (course_id[0], name, curr_date))
+                    attendance_counter = cur.fetchone()
+                    if attendance_counter == (0,):
+                        cur.execute("Insert into Attendance values(?,?,?,?)",
+                                    (course_id[0], name, time_stamp, calendar.day_name[(time_stamp).weekday()]))
+                        db.commit()
+                    else:
+                        pass
+
+                db.commit()
                 cur.close()
-                cur2.close()
+                # db.close()
+
+
+
+
+
+                # print (datetime.date(datetime.now()))
+                # for row in classes:
+                #     abc = []
+                #     abc.append(row)
+                #     print (abc)
+
+
+
+                # Classes = [dict(name=row[0], ph_no=row[1]) for row in c.fetchall()]
+
+                # cur.execute("Create table if not exists Names (name TEXT, Timestamp DATETIME)")
+                # db.commit()
+                # cur.execute("select count(*) from (select name from Names where name =?)", ((hit['name']),))
+                # cur2 = db.cursor()
+                # self._log.info(name)
+
+
+                # cur2.execute("select count(*) from (select ULID,TimeStamp from Attendance where ULID =? and "
+                #              "time(TimeStamp) between time(?) and time(?))", (name,time_stamp + timedelta(minutes=-1),time_stamp + timedelta(hours=1.5)))
+                # name_counter = cur2.fetchone()
+                # print("name_counter: ", name_counter)
+                #
+                # # self._log.info(name_counter)
+                # if name_counter == (0,):
+                #     # cur.execute("Insert into Names values(?,?)", (hit['name'], time_stamp))
+                #     cur2.execute("Insert into Attendance (ULID, TimeStamp, Day) values(?,?,?)", (name, time_stamp, calendar.day_name[(time_stamp).weekday()]))
+                #     db.commit()
+                # else:
+                #     pass
+                # cur.close()
+                # cur2.close()
         except sqlite3.Error as e:
             self._log.error(str(e))
             raise e
